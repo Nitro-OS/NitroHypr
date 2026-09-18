6ba766782b9c71c8b1ca8849383da72e41a67d3d
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-09-18 16:52:51 +0500

    fix: numpad does not work in hyprlock

--- hypr/nitro/input.lua
+++ hypr/nitro/input.lua
@@ -9,7 +9,7 @@ hl.config({
     kb_layout    = "us",
     kb_variant   = "",
     kb_model     = "",
-    kb_options   = "",
+    kb_options   = "numpad:mac",
     kb_rules     = "",
     follow_mouse = 1,
     sensitivity  = 0,
